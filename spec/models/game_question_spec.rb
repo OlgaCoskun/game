adf80322c56d91588276a77b9fdae3264b58da73
Olga Coskun <ovin999@gmail.com>
2018-06-02 09:20:17 +0300

    Fix test

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -36,8 +36,8 @@ RSpec.describe GameQuestion, type: :model do
 
 
   # Задание 61-5
-  context 'game answer' do
-    it 'correct .correct_answer_key' do
+  context '.correct_answer_key' do
+    it 'check correct answer key ' do
       expect(game_question.correct_answer_key).to eq('b')
     end
   end
