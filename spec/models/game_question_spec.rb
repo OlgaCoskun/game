8c9df10e3242f126ed2742c27271130bc2456d76
Olga Coskun <ovin999@gmail.com>
2018-06-05 16:14:13 +0300

    61-5 test

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -36,8 +36,8 @@ RSpec.describe GameQuestion, type: :model do
 
 
   # Задание 61-5
-  context '#correct_answer_key' do
-    it 'back true key ' do
+  context 'when #correct_answer_key' do
+    it 'return true' do
       expect(game_question.correct_answer_key).to eq('b')
     end
   end
